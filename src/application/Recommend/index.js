4e9408e5e76b13ce4ef8b56ccbfcfa3a8d5568cf
DESKTOP-PNHKH71\Administrator <3235413554@qq.com>
2020-08-13 10:22:22 +0800

    feat✨增加  recommendList

--- src/application/Recommend/index.js
+++ src/application/Recommend/index.js
@@ -1,16 +1,27 @@
 //src/appliction/Recommend/index.js
 import React from 'react';
 import Slider from '../../components/slider'
+import RecommendList from '../../components/list'
 
 function Recommend (props) {
 
-  //mock 数据
+  //mock 
   const bannerList = [1,2,3,4].map (item => {
     return { imageUrl: "http://p1.music.126.net/W58ZoucI02Gso-e4byfohw==/109951165229451930.jpg?imageView&quality=89" }
   });
+  const recommendList = [1,2,3,4,5,6,7,8,9,10].map (item => {
+    return {
+      id: 1,
+      picUrl: "https://p1.music.126.net/fhmefjUfMD-8qtj3JKeHbA==/18999560928537533.jpg",
+      playCount: 17171122,
+      name: "朴树、许巍、李健、郑钧、老狼、赵雷"
+    }
+  });
+
   return (
     <div>
       <Slider bannerList={bannerList}></Slider>
+      <RecommendList recommendList={recommendList}></RecommendList> 
     </div>
   )
 }
